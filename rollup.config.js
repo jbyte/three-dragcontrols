a4d12340db39946a59562d43f823c796ffb6c1e3
Jaka Vute <jaka.vute@gmail.com>
2018-04-19 13:20:59 +0200

    Add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel'
+
+const license = `/*!
+    * drag-controls
+    * https://github.com/jbyte/three-dragcontrols
+    * (c) 2018 @jbyte
+    * Released under the MIT License.
+    */`
+
+export default {
+    input: "src/drag-controls.js",
+    plugins: [
+        babel({
+            exclude: "node_modules/**",
+            presets: [
+                ["env", {
+                    targets: {
+                        browsers: [
+                            "last 2 versions",
+                            "ie >= 11"
+                        ]
+                    },
+                    loose: true,
+                    modules: false
+                }]
+            ]
+        })
+    ],
+    output: [{
+        format: "umd",
+        moduleName: "DragControls",
+        file: "build/drag-controls.js",
+        name: "DragControls",
+        banner: license
+    },
+    {
+        format: "es",
+        file: "build/drag-controls.module.js",
+        banner: license
+    }]
+};
